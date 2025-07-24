aefd59f093a5b959700eb08529a6233a3ed417b4
Fabio Delong <frdelong@gmail.com>
2025-07-24 17:12:56 -0300

    feat(products): implement product retrieval and creation with validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,35 @@
 import { NextFunction, Request, Response } from "express"
-import { AppError } from "@/utils/AppError"
+import { knex } from "@/database/knex"
+import { z } from "zod"
 
 class ProductController {
-  async index(request: Request, response: Response, next: NextFunction){
+  async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "Ok!" })
+      const { name } = request.query
+
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name")
+
+      return response.json(products)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").insert({ name, price })
+
+      return response.status(201).json()
     } catch (error) {
       next(error)
     }
